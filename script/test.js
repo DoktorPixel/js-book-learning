4376fbafb6f6f99dcc92a6e9327bd5d9a89cd7aa
unknown <miamby111@gmail.com>
2022-12-15 23:32:15 +0200

    test

--- script/test.js
+++ script/test.js
@@ -1,1 +1,19 @@
 console.log("It is a test page");
+x = 2;
+n = 3;
+function pow(x, n) {
+  let result = 1;
+  // <--
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+  // <--
+  return result;
+  console.log(x, n);
+}
+
+const user = {
+  name: "John",
+};
+user.name = "Pete"; // (*)
+alert(user.name); // Pete
